314402668fd6f17f42a2903ec74904c3b6e53c9e
Lynn Samuelson <lynn.samuelson@gmail.com>
2020-11-03 11:42:59 -0600

    basic project set-up

---
+++ src/components/Help.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>Help Bar</h1>
+</template>
+
+<script>
+
+export default {
+  name: 'Help',
+}
+
+</script>
---
+++ src/components/Info.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>Info Bar</h1>
+</template>
+
+<script>
+
+export default {
+  name: 'Info',
+}
+
+</script>
