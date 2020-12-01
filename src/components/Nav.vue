e69dd515990f2f5258e84c0908da9e264de18aa9
Connor Blakeney <connorblakeney@yahoo.com>
2020-12-01 08:43:55 -0600

    added a link to the nav bar components. all work except for the form for get masks

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,5 +1,14 @@
 <template>
   <h1>Nav Bar</h1>
+    <nav class="nav-bar">
+        <ul>
+            <li><a href="https://masksnow.org/">National Website</a></li>
+            <li><a href="https://charity.gofundme.com/o/en/campaign/masksnow-tennessee">Donate Funds</a></li>
+            <li><a href = "mailto: distribution.make@gmail.com">Donate Materials</a></li>
+            <li><a href="https://masksnow.org/volunteer/">Volunteer</a></li>
+            <li><a href="about.asp">Get Masks</a></li>
+        </ul>
+      </nav>
 </template>
 
 <script>
