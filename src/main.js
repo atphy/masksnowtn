bf0557e5465fa69d12c5b916f5518647344ea3f4
Connor Blakeney <connorblakeney@yahoo.com>
2021-01-25 15:24:30 -0600

    added code to main.js for vuetify

--- src/main.js
+++ src/main.js
@@ -1,5 +1,19 @@
-import { createApp } from 'vue'
+import { createApp, Vue } from 'vue'
 import App from './App.vue'
 import router from './router'
+import vuetify from 'vue-cli-plugin-vuetify'
 
 createApp(App).use(router).mount('#app')
+
+createApp(App)
+  .use(router)
+//   .use(store)
+  .mount("#app");
+
+// I don't use it by default.
+new Vue({
+  router,
+//   store,
+  vuetify,
+  render: h => h(App)
+}).$mount("#app");
