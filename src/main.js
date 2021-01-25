fa2943d065afae283550e90b611fc5ff0a78f0ce
Connor Blakeney <connorblakeney@yahoo.com>
2021-01-25 15:49:17 -0600

    think it installed properly?

--- src/main.js
+++ src/main.js
@@ -1,18 +1,17 @@
 import { createApp, Vue } from 'vue'
 import App from './App.vue'
 import router from './router'
+import vuetify from './plugins/vuetify';
 
 createApp(App).use(router).mount('#app')
 
 createApp(App)
   .use(router)
-//   .use(store)
   .mount("#app");
 
 // I don't use it by default.
 new Vue({
   router,
-//   store,
-//   vuetify,
+  vuetify,
   render: h => h(App)
 }).$mount("#app");
